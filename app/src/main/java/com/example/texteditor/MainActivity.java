2db7787a9694c6655442f4e125cb544b902ea19e
Jisan129 <jishananam@iut-dhaka.edu>
2020-05-13 21:02:40 +0600

    implementing the UI (noob)

--- app/src/main/java/com/example/texteditor/MainActivity.java
+++ app/src/main/java/com/example/texteditor/MainActivity.java
@@ -3,12 +3,18 @@ package com.example.texteditor;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    private EditText editText;
+    private TextView instruction;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        editText=findViewById(R.id.typehere);
+        instruction=findViewById(R.id.instruction);
     }
 }
